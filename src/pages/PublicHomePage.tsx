3ce2b736edf784c662528e6f4362faafdb1692b7
galicioriefonseca-projetos <galicioriefonseca@gmail.com>
2026-09-11 08:58:48 -0300

    fix(landing): avoid duplicated pricing section on public home

--- src/pages/PublicHomePage.tsx
+++ src/pages/PublicHomePage.tsx
@@ -1,13 +1,5 @@
 import LandingPage from './LandingPage';
-import PricingPage from './PricingPage';
 
 export default function PublicHomePage() {
-  return (
-    <>
-      <LandingPage />
-      <section id="planos-publicos" aria-label="Planos LumièreOS">
-        <PricingPage />
-      </section>
-    </>
-  );
+  return <LandingPage />;
 }
